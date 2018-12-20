4c9485002a550a12ac730f0c4e6f9c34ede6225c
Jordan Porter <jordan.1.porter@hotmail.co.uk>
2018-12-20 16:47:26 +0000

    Adding Build Controls
    Create Add & remove handlers. Calculate pricing of burgers etc.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,19 +5,23 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
 
+
      //    js Object to extract keys of the object passed in. This will give us the array of ingredients. 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(ingredientKey => {
-
-
         // The map method is used to execute a function on each element in the array 
         return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
         // Use the spread (...) operator and Array method to create an array. 
         // The map method is used to map the elements to each value. and then returns the burger ingredient component.
                return  <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />;
         });
-    });
+    }).reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
     //End of transformIngredients function 
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
 
         return(
             <div className={classes.Burger}>
